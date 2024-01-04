784278cdc857b143fd86438462a375119afad36f
flucium <flucium@flucium.net>
2024-01-04 14:28:42 +0900

    Create account.py

---
+++ account.py
@@ -0,0 +1,23 @@
+
+# Force account deletion
+def delete():
+  pass
+
+# Account disable
+def diable():
+  pass
+
+# Account enable
+def enable():
+  pass
+
+# Is account enable
+def is_enabled():
+  pass
+
+# Account is exists
+def is_exists():
+  pass
+
+if __name__ == '__main__':
+  pass
